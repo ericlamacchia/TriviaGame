58b354de20717eddace23ef1515ef2edac8083ab
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-09-11 17:14:10 -0400

    Made the function to load my question onto the page and added the jquery to load the timer on the page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,23 @@
-console.log( $ );
+// Need to define my lets these will keep track of the various questions correct questions and incorrect questions
+
+
+let counter = 30;
+let currentQuestion = 0;
+let score = 0;
+let lost = 0;
+// need to create one for keeping track of the time
+let timer;
+
+// I need to be able to display my questions as well as my answers
+function loadQuestion() {
+    const question = quizQuestions[currentQuestion].question;
+    const choices = quizQuestions[currentQuestion].choices;
+// Here I will be creating my timer
+$('#time').html('Timer: ' +counter);
+    $('#game').html(`
+    <h3>${question}</h3>
+    `);
+
+}
+//This is where I need to load my questions
+loadQuestion();
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,8 +1,7 @@
-const quizQuestions = [
-    {
+const quizQuestions = [{
         question: 'What type of farm does Dwight own?',
         choices: ["Bear Farm", "Beat Farm", "Apple Farm", "Banana Farm"],
-        correctAnswer: "Beat Farm"
+        rightAnswer: "Beat Farm"
 
 
 
@@ -10,7 +9,7 @@ const quizQuestions = [
     {
         question: 'How long were Pam and Roy engaged?',
         choices: ["3 Years", "6 Years", "24 Months", "2 Years"],
-        correctAnswer: "3 Years"
+        rightAnswer: "3 Years"
 
 
 
@@ -18,7 +17,7 @@ const quizQuestions = [
     {
         question: 'Which of Angelas cats does dwight kill?',
         choices: ["Bandito", "Stinky", "Sprinkles", "Captain"],
-        correctAnswer: "Sprinkles"
+        rightAnswer: "Sprinkles"
 
 
 
@@ -26,7 +25,7 @@ const quizQuestions = [
     {
         question: 'Who started the fire in The Office?',
         choices: ["Ryan", "Kevin", "IT guy", "Jim"],
-        correctAnswer: "Ryan"
+        rightAnswer: "Ryan"
 
 
 
@@ -34,7 +33,7 @@ const quizQuestions = [
     {
         question: 'What type of car does Dwight drive?',
         choices: ["Spaceship", "Vespa", "Pontiac Trans Am", "Camaro"],
-        correctAnswer: "Pontiac Trans Am"
+        rightAnswer: "Pontiac Trans Am"
 
 
 
@@ -42,7 +41,7 @@ const quizQuestions = [
     {
         question: 'What is Scrantons Nickname?',
         choices: ["Oh my god this town is on fire", "Beetlejuice Village", "The Sad city", "The Electric City"],
-        correctAnswer: "The Electric City"
+        rightAnswer: "The Electric City"
 
 
 
@@ -50,7 +49,7 @@ const quizQuestions = [
     {
         question: 'Whose mother does Michael date?',
         choices: ["Your mom", "Angela", "Jim", "Pam"],
-        correctAnswer: "Pam"
+        rightAnswer: "Pam"
 
 
 
@@ -58,7 +57,7 @@ const quizQuestions = [
     {
         question: 'Which office employee did michael hit with his car?',
         choices: ["Meredith", "Himself", "Jim", "IT guy"],
-        correctAnswer: "Beat Farm"
+        rightAnswer: "Beat Farm"
 
 
 
