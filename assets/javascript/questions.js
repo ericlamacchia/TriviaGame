a15ea0ee638fe131cf5dc39a6cbb0f97e815a83f
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-09-11 16:53:47 -0400

    Made all of my questions as well as added the answers

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,1 +1,66 @@
-console.log('Questions');
+const quizQuestions = [
+    {
+        question: 'What type of farm does Dwight own?',
+        choices: ["Bear Farm", "Beat Farm", "Apple Farm", "Banana Farm"],
+        correctAnswer: "Beat Farm"
+
+
+
+    },
+    {
+        question: 'How long were Pam and Roy engaged?',
+        choices: ["3 Years", "6 Years", "24 Months", "2 Years"],
+        correctAnswer: "3 Years"
+
+
+
+    },
+    {
+        question: 'Which of Angelas cats does dwight kill?',
+        choices: ["Bandito", "Stinky", "Sprinkles", "Captain"],
+        correctAnswer: "Sprinkles"
+
+
+
+    },
+    {
+        question: 'Who started the fire in The Office?',
+        choices: ["Ryan", "Kevin", "IT guy", "Jim"],
+        correctAnswer: "Ryan"
+
+
+
+    },
+    {
+        question: 'What type of car does Dwight drive?',
+        choices: ["Spaceship", "Vespa", "Pontiac Trans Am", "Camaro"],
+        correctAnswer: "Pontiac Trans Am"
+
+
+
+    },
+    {
+        question: 'What is Scrantons Nickname?',
+        choices: ["Oh my god this town is on fire", "Beetlejuice Village", "The Sad city", "The Electric City"],
+        correctAnswer: "The Electric City"
+
+
+
+    },
+    {
+        question: 'Whose mother does Michael date?',
+        choices: ["Your mom", "Angela", "Jim", "Pam"],
+        correctAnswer: "Pam"
+
+
+
+    },
+    {
+        question: 'Which office employee did michael hit with his car?',
+        choices: ["Meredith", "Himself", "Jim", "IT guy"],
+        correctAnswer: "Beat Farm"
+
+
+
+    },
+]
